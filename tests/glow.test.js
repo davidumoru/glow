c4d8b117a280a599a2d58e67af66fbb1c264dfd9
David Umoru <umorudavido@gmail.com>
2023-11-18 11:37:41 +0100

    Add test script

---
+++ tests/glow.test.js
@@ -0,0 +1,19 @@
+const glow = require("../src/index");
+
+describe("glow function", () => {
+  test("applies text style correctly", () => {
+    const result = glow("Test Text", "bright");
+    expect(result).toBe("\x1b[1mTest Text\x1b[0m");
+  });
+
+  test("applies foreground color correctly", () => {
+    const result = glow("Test Text", "fgGreen");
+    expect(result).toBe("\x1b[32mTest Text\x1b[0m");
+  });
+
+  test("applies background color correctly", () => {
+    const result = glow("Test Text", "bgBlue");
+    expect(result).toBe("\x1b[44mTest Text\x1b[0m");
+  });
+
+});
