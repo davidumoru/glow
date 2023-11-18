bdaab8e0446211cd9f1a9ccaaae4df48725145a6
David Umoru <umorudavido@gmail.com>
2023-11-18 15:09:55 +0100

    Adjust styles to use dictionary

--- tests/console.test.js
+++ tests/console.test.js
@@ -5,9 +5,9 @@ console.log(glowr("This text has a 'bright' text styling", "bright"))
 console.log(glowr("This text has a 'reverse' text styling", "reverse"))
 
 // Foreground Colors
-console.log(glowr("This text has a Cyan Foregroung", "fgCyan"))
-console.log(glowr("This text has a Green Foregroung", "fgGreen"))
+console.log(glowr("This text has a Cyan Foregroung", "fg.cyan"))
+console.log(glowr("This text has a Green Foregroung", "fg.green"))
 
 // Background Colors
-console.log(glowr("This text has a Blue Background", "bgBlue"))
-console.log(glowr("This text has a Red Background", "bgRed"))
+console.log(glowr("This text has a Blue Background", "bg.blue"))
+console.log(glowr("This text has a Red Background", "bg.red"))
