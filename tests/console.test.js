a47f585e98fc32c37e129e8f69d9ce8eb22c5100
David Umoru <umorudavido@gmail.com>
2023-11-18 18:18:41 +0100

    Fix typo in "*ground"

--- tests/console.test.js
+++ tests/console.test.js
@@ -10,21 +10,21 @@ console.log(glowr("This text has a 'reverse' text styling", "reverse"));
 console.log(glowr("This text has a 'hidden' text styling", "hidden"));
 
 // Foreground Colors
-console.log(glowr("This text has a Black Foregroung", "fg.black"));
-console.log(glowr("This text has a Red Foregroung", "fg.red"));
-console.log(glowr("This text has a Green Foregroung", "fg.green"));
-console.log(glowr("This text has a Yellow Foregroung", "fg.yellow"));
-console.log(glowr("This text has a Blue Foregroung", "fg.blue"));
-console.log(glowr("This text has a Magenta Foregroung", "fg.magenta"));
-console.log(glowr("This text has a Cyan Foregroung", "fg.cyan"));
-console.log(glowr("This text has a White Foregroung", "fg.white"));
+console.log(glowr("This text has a Black Foreground", "fg.black"));
+console.log(glowr("This text has a Red Foreground", "fg.red"));
+console.log(glowr("This text has a Green Foreground", "fg.green"));
+console.log(glowr("This text has a Yellow Foreground", "fg.yellow"));
+console.log(glowr("This text has a Blue Foreground", "fg.blue"));
+console.log(glowr("This text has a Magenta Foreground", "fg.magenta"));
+console.log(glowr("This text has a Cyan Foreground", "fg.cyan"));
+console.log(glowr("This text has a White Foreground", "fg.white"));
 
 // Background Colors
-console.log(glowr("This text has a Black Backgroung", "bg.black"));
-console.log(glowr("This text has a Red Backgroung", "bg.red"));
-console.log(glowr("This text has a Green Backgroung", "bg.green"));
-console.log(glowr("This text has a Yellow Backgroung", "bg.yellow"));
-console.log(glowr("This text has a Blue Backgroung", "bg.blue"));
-console.log(glowr("This text has a Magenta Backgroung", "bg.magenta"));
-console.log(glowr("This text has a Cyan Backgroung", "bg.cyan"));
-console.log(glowr("This text has a White Backgroung", "bg.white"));
+console.log(glowr("This text has a Black Background", "bg.black"));
+console.log(glowr("This text has a Red Background", "bg.red"));
+console.log(glowr("This text has a Green Background", "bg.green"));
+console.log(glowr("This text has a Yellow Background", "bg.yellow"));
+console.log(glowr("This text has a Blue Background", "bg.blue"));
+console.log(glowr("This text has a Magenta Background", "bg.magenta"));
+console.log(glowr("This text has a Cyan Background", "bg.cyan"));
+console.log(glowr("This text has a White Background", "bg.white"));
