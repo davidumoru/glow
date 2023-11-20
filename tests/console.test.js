528be0cc9f6ccc3073ed399225e5982176475242
David Umoru <umorudavido@gmail.com>
2023-11-20 13:36:45 +0100

    Edit the text logs

--- tests/console.test.js
+++ tests/console.test.js
@@ -1,23 +1,23 @@
 const glowr = require("../index");
 
 // Text Style
-console.log(glowr("This text has a 'reset' text styling", "reset"));
-console.log(glowr("This text has a 'bright' text styling", "bright"));
-console.log(glowr("This text has a 'dim' text styling", "dim"));
-console.log(glowr("This text has a 'underline' text styling", "underline"));
-console.log(glowr("This text has a 'blink' text styling", "blink"));
-console.log(glowr("This text has a 'reverse' text styling", "reverse"));
-console.log(glowr("This text has a 'hidden' text styling", "hidden"));
+console.log(glowr("This text is not styled", "reset"));
+console.log(glowr("This text is bright", "bright"));
+console.log(glowr("This text is dim", "dim"));
+console.log(glowr("This text is underlined", "underline"));
+console.log(glowr("This text keeps blinking", "blink"));
+console.log(glowr("This text has a reverse colour styling", "reverse"));
+console.log(glowr("This text is hidden", "hidden"));
 
 // Foreground Colors
-console.log(glowr("This text has a Black Foreground", "fg.black"));
-console.log(glowr("This text has a Red Foreground", "fg.red"));
-console.log(glowr("This text has a Green Foreground", "fg.green"));
-console.log(glowr("This text has a Yellow Foreground", "fg.yellow"));
-console.log(glowr("This text has a Blue Foreground", "fg.blue"));
-console.log(glowr("This text has a Magenta Foreground", "fg.magenta"));
-console.log(glowr("This text has a Cyan Foreground", "fg.cyan"));
-console.log(glowr("This text has a White Foreground", "fg.white"));
+console.log(glowr("This text is Black", "fg.black"));
+console.log(glowr("This text is Red", "fg.red"));
+console.log(glowr("This text is Green", "fg.green"));
+console.log(glowr("This text is Yellow", "fg.yellow"));
+console.log(glowr("This text is Blue", "fg.blue"));
+console.log(glowr("This text is Magenta", "fg.magenta"));
+console.log(glowr("This text is Cyan", "fg.cyan"));
+console.log(glowr("This text is White", "fg.white"));
 
 // Background Colors
 console.log(glowr("This text has a Black Background", "bg.black"));
